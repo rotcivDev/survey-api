2234167905a42619ab91ce07a275bb7130470b0d
Victor Salles <vsallesa@gmail.com>
2024-02-23 13:26:46 -0300

    chore: add and config jest

---
+++ jest.config.js
@@ -0,0 +1,17 @@
+/**
+ * For a detailed explanation regarding each configuration property, visit:
+ * https://jestjs.io/docs/configuration
+ */
+
+/** @type {import('jest').Config} */
+module.exports = {
+  roots: ['<rootDir>/src'],
+  collectCoverage: true,
+  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  coverageDirectory: 'coverage',
+  coverageProvider: 'v8',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.ts$': 'ts-jest'
+  }
+}
