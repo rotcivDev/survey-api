173d7ea6d4abd9d05f8dec4df445117f100de285
Victor Salles <vsallesa@gmail.com>
2024-02-23 20:06:17 -0300

    feat: ensure SignUpController return 400 if no email is provided

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,7 +1,17 @@
 export class SignUpController {
   handle (httpRequest: any): any {
-    return {
-      statusCode: 400
+    if (!httpRequest.body.name) {
+      return {
+        statusCode: 400,
+        body: new Error('Missing param: name')
+      }
+    }
+
+    if (!httpRequest.body.email) {
+      return {
+        statusCode: 400,
+        body: new Error('Missing param: email')
+      }
     }
   }
 }
