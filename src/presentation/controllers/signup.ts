eac44d00a26cca0336cef4cd83c457d7293ff272
Victor Salles <vsallesa@gmail.com>
2024-02-23 21:37:48 -0300

    refactor: remove repeated required fields validation code

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -4,12 +4,11 @@ import { badRequest } from '../helpers/http-helper'
 
 export class SignUpController {
   handle (httpRequest: HttpRequest): HttpResponse {
-    if (!httpRequest.body?.name) {
-      return badRequest(new MissingParamError('name'))
-    }
-
-    if (!httpRequest.body?.email) {
-      return badRequest(new MissingParamError('email'))
+    const REQUIRED_FIELDS = ['name', 'email']
+    for (const field of REQUIRED_FIELDS) {
+      if (!httpRequest.body[field]) {
+        return badRequest(new MissingParamError(field))
+      }
     }
 
     return {
